5f7f741c4433df3edb14450e70a7b3b62c46a0e8
jibinsparksupport <jibin.d@sparksupport.com>
2022-06-05 19:44:58 +0530

    signup

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView
 
+from .views import SignUpView
+
 urlpatterns = [
-    path('', LoginView.as_view(template_name='accounts/login.html'), name='login')
+    path('', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.views.generic import CreateView
+from django.urls import reverse, reverse_lazy
+
+from .forms import UserForm
 
 # Create your views here.
+
+
+class SignUpView(CreateView):
+    model = UserForm
+    form_class = UserForm
+    template_name = 'accounts/signup.html'
+
+    def get_success_url(self):
+        return reverse_lazy('login')
