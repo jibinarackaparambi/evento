af11d8ca9ac63a6f8bba855e9c21ec536aaadde0
jibinsparksupport <jibin.d@sparksupport.com>
2022-06-12 11:30:18 +0530

    event model creation

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.views.generic import CreateView
 from django.urls import reverse, reverse_lazy
+from django.contrib import messages
 
 from .forms import UserForm
 
@@ -13,5 +14,9 @@ class SignUpView(CreateView):
     form_class = UserForm
     template_name = 'accounts/signup.html'
 
+    def form_valid(self, form):
+        messages.success(self.request, "Your account successfully created.")
+        return super().form_valid(form)
+
     def get_success_url(self):
-        return reverse_lazy('login')
+        return reverse_lazy('signup')
--- events/models.py
+++ events/models.py
@@ -1,3 +1,12 @@
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=132)
+    location_name = models.CharField(max_length=132)
+    date = models.DateTimeField()
+    time = models.TimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    price = models.FloatField(default=0.0)
